1b33c6cf69f601f874e5f23ea00e46a7ea8d8f40
David Guerrero <deivo.guerrero@gmail.com>
2026-04-28 11:47:19 -0500

    Chee ya que no me dejan instalar aplicaciones de la tienda de Microsoft y tengo bloqueado todo me toco construir un script (con la ayuda de ChatGPT obvio) que mueve el mouse en un circulo cada 20 segundos, le puedes configurar el radio, tiempo que se demore haciendo el círculo y el intervalo del moviemiento en las variables de las lineas 7, 8 y 9 respectivamanete. No necesita instalar librerías adicionales ya las incorpora python 3 nativamente

---
+++ main.py
@@ -0,0 +1,36 @@
+import ctypes
+import time
+import math
+
+user32 = ctypes.windll.user32
+
+radio = 20
+time_step = 0.003
+interval = 20
+
+
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+
+def get_mouse_position():
+    pt = POINT()
+    user32.GetCursorPos(ctypes.byref(pt))
+    return pt.x, pt.y
+
+
+def move_mouse(x, y):
+    user32.SetCursorPos(int(x), int(y))
+
+
+while True:
+    x0, y0 = get_mouse_position()
+
+    for angulo in range(0, 360, 5):
+        rad = math.radians(angulo)
+        x = x0 + radio * math.cos(rad)
+        y = y0 + radio * math.sin(rad)
+        move_mouse(x, y)
+        time.sleep(time_step)
+
+    time.sleep(interval)
